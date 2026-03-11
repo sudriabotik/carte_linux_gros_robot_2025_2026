5c158cd8a9e766e0275c187b752b780e78f2a606
BOX_268 <119125853+BOX268@users.noreply.github.com>
2026-03-11 14:46:51 +0100

    - added some canopen commands in comm_asserv

--- comm_asserv.py
+++ comm_asserv.py
@@ -2,11 +2,24 @@
 Contains functions allowing to send the correct action_id and arguments
 at the correct address to control the card.
 """
+import enum
 
 import canopen_wrapper
 import canopen
 
 from action_comm_node import CanActionNode
+
+class Face(enum.Enum) :
+
+	FACE_AVANT = 0
+	FACE_ARRIERE = 1
+
+
+class Facing(enum.Enum) :
+	POSITIVE_X = 0
+	POSITIVE_Y = 1
+	NEGATIVE_X = 2
+	NEGATIVE_Y = 3
 
 
 # all the state enum from the C code
@@ -17,6 +30,27 @@ class CanAsservNode(CanActionNode) :
 		super().__init__(network,  node_id, dictionary_path)
 	
 
+	def action_set_linear_speed_accel(self, speed: int, acceleration : int) :
+		"""
+		performs the homing (initialisation of the motor positions using the switches)
+		The speed is in thousandths of mm/ms
+		The velocity is in thousandths of mm/ms^2
+		"""
+		canopen_wrapper.instance.request_action(self.node, 1, [speed, acceleration])
+	
+	def action_set_angular_speed_accel(self, speed: int, acceleration : int) :
+		"""
+		performs the homing (initialisation of the motor positions using the switches)
+		The speed is in thousandths of deg/ms
+		The velocity is in thousandths of deg/ms^2
+		"""
+		canopen_wrapper.instance.request_action(self.node, 2, [speed, acceleration])
+	
+	def action_goto_xy(self, x_mm: int, y_mm : int, face : Face) :
+		"""
+		Goes to the given point, using the requested face
+		"""
+		canopen_wrapper.instance.request_action(self.node, 5, [x_mm, y_mm, face])
 
 	def action_translation(self, distance_mm: int, speed : int, accel : int) :
 		"""
@@ -24,12 +58,12 @@ class CanAsservNode(CanActionNode) :
 		The speed is in thousandths of mm/ms
 		The acceleration is in thousandths of mm/ms^2
 		"""
-		canopen_wrapper.instance.request_action(self.node, 1, [distance_mm, speed, accel])
+		canopen_wrapper.instance.request_action(self.node, 3, [distance_mm, speed, accel])
 	
 
-	def action_goto_xy(self, x_mm: int, y_mm : int) :
+	def action_recalibration(self, facing : Facing, face : Face) :
 		"""
-		performs the homing (initialisation of the motor positions using the switches)
+		Resets a coordinate of the robot using the walls of the table
 		"""
-		canopen_wrapper.instance.request_action(self.node, 3, [x_mm, y_mm])
+		canopen_wrapper.instance.request_action(self.node, 6, [facing, face])
 
