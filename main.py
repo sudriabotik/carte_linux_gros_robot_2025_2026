c9e2fa2f0adcba16441eb589bad4cec41ba5d0af
Paul <vitalium.02@gmail.com>
2026-03-19 14:30:48 +0100

    fixed typos

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/bin/env python3
+##!/bin/env python3
 
 import can
 import can.interfaces.serial.serial_can
@@ -14,14 +14,14 @@ import logger
 
 print("attempting to connect to the bus")
 # bus : can.BusABC = can.interfaces.serial.serial_can.SerialBus(channel="/dev/ttyACM0", baudrate=500000)
-bus = can.Bus(channel="can0", interface="socketcan")
+bus = can.Bus(channel="com12", interface="slcan")
 print("connected to the network")
 
 canopen_wrapper.instance = canopen_wrapper.CanopenWrapper(bus)
 
 node_autom = None
 try :
-    node_autom = comm_autom.CanAsservNode(bus, 1)
+    node_autom = comm_autom.CanAutomNode(bus, 2)
 except Exception as e :
     logger.log_error("Main", f"cannot create autom node, {e}")
 
@@ -37,6 +37,8 @@ except Exception as e :
 #node_autom.action_homing(10, 10)
 
 node_asserv.action_recalibration(comm_asserv.Facing.NEGATIVE_X, comm_asserv.Face.FACE_ARRIERE)
+while (node_asserv.is_busy()) : time.sleep(0.05)
+node_asserv.action_translation(100, 10, 10)
 while (node_asserv.is_busy()) : time.sleep(0.05)
 node_asserv.action_recalibration(comm_asserv.Facing.POSITIVE_Y, comm_asserv.Face.FACE_ARRIERE)
 while (node_asserv.is_busy()) : time.sleep(0.05)
